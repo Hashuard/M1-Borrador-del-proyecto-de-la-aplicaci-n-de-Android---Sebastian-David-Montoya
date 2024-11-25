4a94047712e9ed54cdbd0ef25f6abb273087d46f
S3bmon <reinososebastian2202@gmail.com>
2024-11-25 01:10:59 -0300

    initial commit

---
+++ app/src/main/java/com/hashuard/myapplicationupdatem5/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hashuard.myapplicationupdatem5 //Sebastian David Montoya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
